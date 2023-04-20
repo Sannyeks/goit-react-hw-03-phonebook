ce46b64879cef76785663fc1f320424044c900a6
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-04-20 18:14:28 +0300

    add new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,88 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { nanoid } from 'nanoid';
+import { Section, Title } from './App.styled';
+
+import Form from './Form';
+import ContactsList from './ContactsList';
+import Filter from './Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const data = JSON.parse(localStorage.getItem('contacts'));
+    if (data) {
+      this.setState({ contacts: data });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  addNewContact = data => {
+    const { contacts } = this.state;
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+
+    contacts.some(({ name }) => name === data.name)
+      ? Notify.warning(`${data.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <>
+        <Section>
+          <Title>Phonebook</Title>
+          <Form addNewContact={this.addNewContact} />
+        </Section>
+        <Section>
+          <Title>Contacts</Title>
+          {contacts.length ? (
+            <>
+              <Filter value={filter} onChange={this.changeFilter} />
+              <ContactsList
+                contacts={visibleContacts}
+                deleteContact={this.deleteContact}
+              />
+            </>
+          ) : (
+            <p>Contact list is ampty</p>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
